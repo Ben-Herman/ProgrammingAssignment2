dacbc1888f23313f4cf57d70a014a5f231cc73a1
Ben-Herman <ben.r.herman@gmail.com>
2016-09-10 21:30:38 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+# A pair of functions that work together to provide a method
+# of storing gettting the inverse of a matrix, either
+# by using the `solve` function, or by retrieving the result of
+# an earlier call to `solve`.  Matrices are represented as a list
+# of functions that get and set the base representation of the matrix and
+# the base representation of its inverse.
+# Functions in the list are as follows:
+# 
+# set(X)            Sets the value of the matrix and clears
+#                   the inverse value in cache
+# 
+# get()             Gets the value of the matrix
+# 
+# setinverse(Xinv)  Sets a value for the inverse matrix in cache
+#                   (The user is responsible for setting the correct value)
+# 
+# getinverse()      Gets the stored value of the inverse matrix
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## Create the list of functions representing X
+makeCacheMatrix <- function(X = matrix()) {
+    Xinv <- NULL
+    set <- function(Xset) {
+        X <<- Xset
+        Xinv <<- NULL
+    }
+    get <- function() X
+    setinverse <- function(inv) Xinv <<- inv
+    getinverse <- function() Xinv
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Returns the inverse of a matrix constructed from an output
+## of the function makeCacheMatrix.  The inverse is computed and 
+## stored if it doesn't alread exist.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cacheMat, ...) {
+    Xinv <- cacheMat$getinverse()
+    if(!is.null(Xinv)) {
+        message("getting cached data")
+        return(Xinv)
+    }
+    X <- cacheMat$get()
+    Xinv <- solve(X, diag(dim(X)[[1]]), ...)
+    # diag(dim(X)[[1]]) gives the identity matrix as the
+    # b input to `solve` thereby allowing ... of `cacheSolve`
+    # to correspond to ... of `solve`
+    cacheMat$setinverse(Xinv)
+    Xinv
 }
